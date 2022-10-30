9d53de18b7837763879979ff44402049bf230c42
Reuel Nixon <reuelnixon@gmail.com>
2022-10-30 10:14:30 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const ejsMate = require('ejs-mate')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+
+app.engine('ejs', ejsMate)
+
+app.get('/', (req, res)=>{
+    res.render('home.ejs')
+})
+
+app.get('/technical', (req, res)=>{
+    res.render('technical.ejs')
+})
+
+app.get('/management', (req, res)=>{
+    res.render('management.ejs')
+})
+
+app.get('/media', (req, res)=>{
+    res.render('media.ejs')
+})
+
+app.get('/content', (req, res)=>{
+    res.render('content.ejs')
+})
+
+app.all('*', (req, res, next)=>{
+    res.render('pageNotFound.ejs')
+})
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+})
+
